b0b9fb14a3b8ca50e55bb172aa128c75c2264777
Vayom PC <korefan639@gmail.com>
2024-04-20 16:34:46 +0300

    Add Review Model for admin panel

--- market/admin.py
+++ market/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from market.models import Category, Product, UserProfile, Cart, Order
+from market.models import Category, Product, UserProfile, Cart, Order, Review
 
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Cart)
 admin.site.register(UserProfile)
 admin.site.register(Order)
+admin.site.register(Review)
