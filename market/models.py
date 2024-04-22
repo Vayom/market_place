582e4d0adaa7bdb1a9b76c5c223a99105b2613b3
Vayom PC <korefan639@gmail.com>
2024-04-22 18:02:16 +0300

    add documentation

--- market/models.py
+++ market/models.py
@@ -5,45 +5,91 @@ from user_auth.models import CustomUser
 
 
 class Product(models.Model):
+    """
+    Model representing a product in the store.
+
+    Attributes:
+        name (str): The name of the product.
+        description (str): The description of the product.
+        price (Decimal): The price of the product.
+        category (Category): The category to which the product belongs.
+        user (CustomUser): The user who added the product.
+    """
+
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 
-    # Добавьте другие поля, такие как изображения и количество на складе, по вашему усмотрению
-
     def __str__(self):
         return self.name
 
 
+
 class Order(models.Model):
+    """
+    Model representing an order in the store.
+
+    Attributes:
+        user (CustomUser): The user who placed the order.
+        products (ManyToManyField): The products included in the order.
+        status (str): The status of the order (e.g., "Pending", "Completed", etc.).
+        total_amount (Decimal): The total amount of the order.
+    """
+
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product)
     status = models.CharField(max_length=50, default="Pending")
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
 
-    # Другие поля, такие как дата и время заказа, могут быть добавлены по вашему усмотрению
-
     def __str__(self):
         return f"Order {self.id} by {self.user.username}"
 
 
+
 class Payment(models.Model):
+    """
+    Model representing a payment in the store.
+
+    Attributes:
+        order (Order): The order associated with the payment.
+        payment_method (str): The payment method.
+        amount_paid (Decimal): The amount paid.
+    """
+
     order = models.OneToOneField(Order, on_delete=models.CASCADE)
     payment_method = models.CharField(max_length=50)
     amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
-    # Другие поля, связанные с платежами, могут быть добавлены по вашему усмотрению
+
 
 
 class Category(models.Model):
+    """
+    Model representing a category of products.
+
+    Attributes:
+        name (str): The name of the category.
+    """
+
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
 
+
 class Review(models.Model):
+    """
+    Model representing a review for a product.
+
+    Attributes:
+        product (Product): The product being reviewed.
+        user (CustomUser): The user who wrote the review.
+        text (str): The text of the review.
+        rating (int): The rating given in the review.
+    """
+
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     text = models.TextField()
@@ -53,7 +99,15 @@ class Review(models.Model):
         return f"Review {self.id} by {self.user} for product {self.product.name}"
 
 
+
 class UserProfile(models.Model):
+    """
+    Model representing a user profile.
+
+    Attributes:
+        user (CustomUser): One-to-one relationship with a user (foreign key).
+        address (str): User's address. Maximum length of 255 characters. Can be blank.
+    """
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     address = models.CharField(max_length=255, null=True, blank=True)
 
@@ -62,6 +116,13 @@ class UserProfile(models.Model):
 
 
 class Cart(models.Model):
+    """
+    Model representing a user's shopping cart.
+
+    Attributes:
+        user (CustomUser): One-to-one relationship with a user (foreign key).
+        products (ManyToManyField): A many-to-many relationship with products.
+    """
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product, null=True, blank=True)
 
@@ -70,6 +131,12 @@ class Cart(models.Model):
 
 
 class PromoCode(models.Model):
+    """
+    Model representing promotional codes.
+
+    Attributes:
+        code (str): The promotional code. Maximum length of 20 characters. Unique.
+        discount_amount (Decimal): The amount of discount provided by the promotional code.
+    """
     code = models.CharField(max_length=20, unique=True)
     discount_amount = models.DecimalField(max_digits=5, decimal_places=2)
-    # Другие поля, связанные с промокодами, могут быть добавлены по вашему усмотрению
